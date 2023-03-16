aed94adc2845fb8bbea55025f0d591f65f2a7784
Pranto Halder <halder2207509@stud.kuet.ac.bd>
2023-03-16 15:24:41 +0600

    feature update : 1.doctor type usermgm part is complete [16.03,2023]

--- gunk/v1/doctortype/all_grpc.pb.go
+++ gunk/v1/doctortype/all_grpc.pb.go
@@ -19,6 +19,10 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type DoctorTypeServiceClient interface {
 	RegisterDoctorType(ctx context.Context, in *RegisterDoctorTypeRequest, opts ...grpc.CallOption) (*RegisterDoctorTypeResponse, error)
+	EditDoctorType(ctx context.Context, in *EditDoctorTypeRequest, opts ...grpc.CallOption) (*EditDoctorTypeResponse, error)
+	UpdateDoctorType(ctx context.Context, in *UpdateDoctorTypeRequest, opts ...grpc.CallOption) (*UpdateDoctorTypeResponse, error)
+	DeleteDoctorType(ctx context.Context, in *DeleteDoctorTypeRequest, opts ...grpc.CallOption) (*DeleteDoctorTypeResponse, error)
+	DoctorTypeList(ctx context.Context, in *DoctorTypeListRequest, opts ...grpc.CallOption) (*DoctorTypeListResponse, error)
 }
 
 type doctorTypeServiceClient struct {
@@ -38,11 +42,51 @@ func (c *doctorTypeServiceClient) RegisterDoctorType(ctx context.Context, in *Re
 	return out, nil
 }
 
+func (c *doctorTypeServiceClient) EditDoctorType(ctx context.Context, in *EditDoctorTypeRequest, opts ...grpc.CallOption) (*EditDoctorTypeResponse, error) {
+	out := new(EditDoctorTypeResponse)
+	err := c.cc.Invoke(ctx, "/doctortypepb.DoctorTypeService/EditDoctorType", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *doctorTypeServiceClient) UpdateDoctorType(ctx context.Context, in *UpdateDoctorTypeRequest, opts ...grpc.CallOption) (*UpdateDoctorTypeResponse, error) {
+	out := new(UpdateDoctorTypeResponse)
+	err := c.cc.Invoke(ctx, "/doctortypepb.DoctorTypeService/UpdateDoctorType", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *doctorTypeServiceClient) DeleteDoctorType(ctx context.Context, in *DeleteDoctorTypeRequest, opts ...grpc.CallOption) (*DeleteDoctorTypeResponse, error) {
+	out := new(DeleteDoctorTypeResponse)
+	err := c.cc.Invoke(ctx, "/doctortypepb.DoctorTypeService/DeleteDoctorType", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *doctorTypeServiceClient) DoctorTypeList(ctx context.Context, in *DoctorTypeListRequest, opts ...grpc.CallOption) (*DoctorTypeListResponse, error) {
+	out := new(DoctorTypeListResponse)
+	err := c.cc.Invoke(ctx, "/doctortypepb.DoctorTypeService/DoctorTypeList", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // DoctorTypeServiceServer is the server API for DoctorTypeService service.
 // All implementations must embed UnimplementedDoctorTypeServiceServer
 // for forward compatibility
 type DoctorTypeServiceServer interface {
 	RegisterDoctorType(context.Context, *RegisterDoctorTypeRequest) (*RegisterDoctorTypeResponse, error)
+	EditDoctorType(context.Context, *EditDoctorTypeRequest) (*EditDoctorTypeResponse, error)
+	UpdateDoctorType(context.Context, *UpdateDoctorTypeRequest) (*UpdateDoctorTypeResponse, error)
+	DeleteDoctorType(context.Context, *DeleteDoctorTypeRequest) (*DeleteDoctorTypeResponse, error)
+	DoctorTypeList(context.Context, *DoctorTypeListRequest) (*DoctorTypeListResponse, error)
 	mustEmbedUnimplementedDoctorTypeServiceServer()
 }
 
@@ -52,6 +96,18 @@ type UnimplementedDoctorTypeServiceServer struct {
 
 func (UnimplementedDoctorTypeServiceServer) RegisterDoctorType(context.Context, *RegisterDoctorTypeRequest) (*RegisterDoctorTypeResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method RegisterDoctorType not implemented")
+}
+func (UnimplementedDoctorTypeServiceServer) EditDoctorType(context.Context, *EditDoctorTypeRequest) (*EditDoctorTypeResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method EditDoctorType not implemented")
+}
+func (UnimplementedDoctorTypeServiceServer) UpdateDoctorType(context.Context, *UpdateDoctorTypeRequest) (*UpdateDoctorTypeResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UpdateDoctorType not implemented")
+}
+func (UnimplementedDoctorTypeServiceServer) DeleteDoctorType(context.Context, *DeleteDoctorTypeRequest) (*DeleteDoctorTypeResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DeleteDoctorType not implemented")
+}
+func (UnimplementedDoctorTypeServiceServer) DoctorTypeList(context.Context, *DoctorTypeListRequest) (*DoctorTypeListResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DoctorTypeList not implemented")
 }
 func (UnimplementedDoctorTypeServiceServer) mustEmbedUnimplementedDoctorTypeServiceServer() {}
 
@@ -84,6 +140,78 @@ func _DoctorTypeService_RegisterDoctorType_Handler(srv interface{}, ctx context.
 	return interceptor(ctx, in, info, handler)
 }
 
+func _DoctorTypeService_EditDoctorType_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(EditDoctorTypeRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(DoctorTypeServiceServer).EditDoctorType(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/doctortypepb.DoctorTypeService/EditDoctorType",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(DoctorTypeServiceServer).EditDoctorType(ctx, req.(*EditDoctorTypeRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _DoctorTypeService_UpdateDoctorType_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UpdateDoctorTypeRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(DoctorTypeServiceServer).UpdateDoctorType(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/doctortypepb.DoctorTypeService/UpdateDoctorType",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(DoctorTypeServiceServer).UpdateDoctorType(ctx, req.(*UpdateDoctorTypeRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _DoctorTypeService_DeleteDoctorType_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DeleteDoctorTypeRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(DoctorTypeServiceServer).DeleteDoctorType(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/doctortypepb.DoctorTypeService/DeleteDoctorType",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(DoctorTypeServiceServer).DeleteDoctorType(ctx, req.(*DeleteDoctorTypeRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _DoctorTypeService_DoctorTypeList_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DoctorTypeListRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(DoctorTypeServiceServer).DoctorTypeList(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/doctortypepb.DoctorTypeService/DoctorTypeList",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(DoctorTypeServiceServer).DoctorTypeList(ctx, req.(*DoctorTypeListRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // DoctorTypeService_ServiceDesc is the grpc.ServiceDesc for DoctorTypeService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -94,6 +222,22 @@ var DoctorTypeService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "RegisterDoctorType",
 			Handler:    _DoctorTypeService_RegisterDoctorType_Handler,
+		},
+		{
+			MethodName: "EditDoctorType",
+			Handler:    _DoctorTypeService_EditDoctorType_Handler,
+		},
+		{
+			MethodName: "UpdateDoctorType",
+			Handler:    _DoctorTypeService_UpdateDoctorType_Handler,
+		},
+		{
+			MethodName: "DeleteDoctorType",
+			Handler:    _DoctorTypeService_DeleteDoctorType_Handler,
+		},
+		{
+			MethodName: "DoctorTypeList",
+			Handler:    _DoctorTypeService_DoctorTypeList_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
--- usermgm/service/doctortype/doctortype.go
+++ usermgm/service/doctortype/doctortype.go
@@ -9,7 +9,11 @@ import (
 )
 
 type CoreDoctorType interface {
-	Registerdoctortype(storage.Doctor_type) (*storage.Doctor_type, error)
+	Registerdoctortype(storage.DoctorType) (*storage.DoctorType, error)
+	EditDoctorType(u storage.Edit) (*storage.DoctorType, error)
+	UpdateDoctorType(u storage.DoctorType) (*storage.DoctorType,error)
+	DeleteDoctorTypeID(u storage.Edit) error
+	ListDoctorType(u storage.UserFilter)([]storage.DoctorType,error)
 }
 
 type DoctorTypeSvc struct {
@@ -25,7 +29,7 @@ func NewDoctorTypeSvc(cu CoreDoctorType) *DoctorTypeSvc {
 
 //doctor type register
 func (us DoctorTypeSvc) RegisterDoctorType(ctx context.Context, r *doctortypepb.RegisterDoctorTypeRequest) (*doctortypepb.RegisterDoctorTypeResponse, error) {
-	user := storage.Doctor_type{
+	user := storage.DoctorType{
 		ID:         int(r.GetID()),
 		DoctorType: r.GetDoctorType(),
 	}
@@ -44,4 +48,87 @@ func (us DoctorTypeSvc) RegisterDoctorType(ctx context.Context, r *doctortypepb.
 			DoctorType: u.DoctorType,
 		},
 	}, nil
+}
+//edit doctor type
+func (us DoctorTypeSvc) EditDoctorType(ctx context.Context,r *doctortypepb.EditDoctorTypeRequest) (*doctortypepb.EditDoctorTypeResponse, error) {
+	user := storage.Edit{
+		ID:         int(r.GetId()),
+	}
+	if err := user.Validate(); err != nil {
+		fmt.Println("the error is in the serveice layer in Register after user.Validate")
+		return nil, err
+	}
+	u, err := us.core.EditDoctorType(user)
+	if err != nil {
+		fmt.Println("the error is in the serveice layer in Register after Register(user)")
+		return nil, err
+	}
+	return &doctortypepb.EditDoctorTypeResponse{
+		User: &doctortypepb.DoctorType{
+			ID: int32(u.ID),
+			 DoctorType: u.DoctorType},
+	}, nil
+
+}
+//update doctor type
+func (us DoctorTypeSvc)UpdateDoctorType(ctx context.Context,r *doctortypepb.UpdateDoctorTypeRequest) (*doctortypepb.UpdateDoctorTypeResponse, error){
+	user := storage.DoctorType{
+		ID:         int(r.GetID()),
+		DoctorType: r.GetDcotorType(),
+		
+	}
+	if err := user.Validate(); err != nil {
+		fmt.Println("the error is in the serveice layer in Register after user.Validate")
+		return nil, err
+	}
+	u, err := us.core.UpdateDoctorType(user)
+	if err != nil {
+		fmt.Println("the error is in the serveice layer in Register after Register(user)")
+		return nil, err
+	}
+	return &doctortypepb.UpdateDoctorTypeResponse{
+		User: &doctortypepb.DoctorType{
+			ID:         int32(u.ID),
+			DoctorType: u.DoctorType,
+		},
+	}, nil
+}
+//delete doctor type
+func (us DoctorTypeSvc)DeleteDoctorType(ctx context.Context,r *doctortypepb.DeleteDoctorTypeRequest) (*doctortypepb.DeleteDoctorTypeResponse, error) {
+	user := storage.Edit{
+		ID:         int(r.GetId()),
+	}
+	if err := user.Validate(); err != nil {
+		fmt.Println("the error is in the serveice layer in Register after user.Validate")
+		return nil, err
+	}
+	if err := us.core.DeleteDoctorTypeID(user);err != nil {
+		return nil,err
+	}
+	return &doctortypepb.DeleteDoctorTypeResponse{},nil
+
+}
+//list doctor type
+func (us DoctorTypeSvc)DoctorTypeList(ctx context.Context,r *doctortypepb.DoctorTypeListRequest) (*doctortypepb.DoctorTypeListResponse, error){
+  user := storage.UserFilter{
+  	SearchTerm: r.GetSearchTerm(),
+  }
+  u,err := us.core.ListDoctorType(user)
+	if err != nil {
+		return nil,err
+	}
+	var totaldoctortype []*doctortypepb.DoctorType
+	for _,value := range u {
+		user:=&doctortypepb.DoctorType{
+			ID:         int32(value.ID),
+			DoctorType: value.DoctorType,
+		}
+		totaldoctortype = append(totaldoctortype,user)
+	}
+   return &doctortypepb.DoctorTypeListResponse{
+   	DcotorTypeFilterList: &doctortypepb.DcotorTypeFilterList{
+   		TotalUser:  totaldoctortype,
+   		SearchTerm: user.SearchTerm,
+   	},
+   },nil
 }
