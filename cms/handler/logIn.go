9af5995913f49503d0123c4ebc1802a74290e474
Pranto Halder <halder2207509@stud.kuet.ac.bd>
2023-03-24 15:53:06 +0600

    quickfix

--- cms/handler/logIn.go
+++ cms/handler/logIn.go
@@ -3,9 +3,11 @@ package handler
 import (
 	"log"
 	"net/http"
+	"strconv"
 
 	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/justinas/nosurf"
+	loginpb "main.go/gunk/v1/login"
 )
 
 type LoginUser struct {
@@ -22,18 +24,99 @@ func (h Handler) Login (w http.ResponseWriter, r *http.Request){
 	})
 }
 
+func (h Handler) ParseLoginTemplates(w http.ResponseWriter, data any) {
+	t := h.Templates.Lookup("login.html")
+	if t == nil {
+		log.Fatal("can not look up login.html template")
+		http.Error(w,"Internal Server Error",http.StatusInternalServerError)
+	}
+	if err := t.Execute(w, data); err != nil {
+		log.Fatal("can not look up login.html template")
+		http.Error(w,"Internal Server Error",http.StatusInternalServerError)
+	}
+}
 
 func (h Handler) LoginPost (w http.ResponseWriter, r *http.Request){
+	if err := r.ParseForm(); err != nil {
+		log.Fatal(err)
+	}
+	var lf LoginUser
+	if err := h.decoder.Decode(&lf, r.PostForm); err != nil {
+		log.Println(err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+	}
+
+	if lf.Loginas == nil {
+		if err := lf.Validate(); err != nil {
+			if vErr, ok := err.(validation.Errors); ok {
+				lf.FormError = vErr
+			}
+			h.ParseLoginTemplates(w, LoginUser{
+				Username:  lf.Username,
+				Password:  "",
+				FormError: lf.FormError,
+				CSRFToken: nosurf.Token(r),
+			})
+			return
+		}
+	}
 	
+	for _, value := range lf.Loginas {
+		if value == "Patient" {
+			u,err := h.usermgmService.Login(r.Context(),&loginpb.LoginRequest{
+				Username: lf.Username,
+				Password: lf.Password,
+			})
+			if err != nil {
+				log.Println("the error is in the login section of cms in patient after h.usermgmService.Login")
+				http.Error(w, "internal server error", http.StatusInternalServerError)
+				return
+			}
+			h.sessionManager.Put(r.Context(), "userID", strconv.Itoa(int(u.GetUser().ID)))
+	        http.Redirect(w, r, "/patients/home", http.StatusSeeOther)
+		}
+	}
 	
+	for _, value := range lf.Loginas {
+		if value == "Admin" {
+			u,err := h.usermgmService.Login(r.Context(),&loginpb.LoginRequest{
+				Username: lf.Username,
+				Password: lf.Password,
+			})
+			if err != nil {
+				log.Println("the error is in the login section of cms in patient after h.usermgmService.Login")
+				http.Error(w, "internal server error", http.StatusInternalServerError)
+				return
+			}
+			h.sessionManager.Put(r.Context(), "userID", strconv.Itoa(int(u.GetUser().ID)))
+	        http.Redirect(w, r, "/admin/home", http.StatusSeeOther)
+		}
+	}
+
+	for _, value := range lf.Loginas {
+		if value == "Doctor" {
+			u,err := h.usermgmService.Login(r.Context(),&loginpb.LoginRequest{
+				Username: lf.Username,
+				Password: lf.Password,
+			})
+			if err != nil {
+				log.Println("the error is in the login section of cms in patient after h.usermgmService.Login")
+				http.Error(w, "internal server error", http.StatusInternalServerError)
+				return
+			}
+			h.sessionManager.Put(r.Context(), "userID", strconv.Itoa(int(u.GetUser().ID)))
+	        http.Redirect(w, r, "/doctor/home", http.StatusSeeOther)
+		}
+	}
+	h.ParseLoginTemplates(w, nil)
 }
 
 func (lu LoginUser) Validate() error {
 	return validation.ValidateStruct(&lu, validation.Field(&lu.Username,
 		validation.Required.Error("username can not be blank"),
 	),
-	validation.Field(&lu.Password,
-		validation.Required.Error("password can not be blank"),
+		validation.Field(&lu.Password,
+			validation.Required.Error("password can not be blank"),
 		),
 		validation.Field(&lu.Loginas,
 			validation.Required.Error("login role can not be blank"),
@@ -57,13 +140,4 @@ func (h Handler) LogoutAdminhandler (w http.ResponseWriter, r *http.Request){
 		log.Fatal(err)
 	}
 	http.Redirect(w,r,"/login",http.StatusSeeOther)
-}
-func (h Handler) ParseLoginTemplates(w http.ResponseWriter, data any) {
-	t := h.Templates.Lookup("login.html")
-	if t == nil {
-		http.Error(w,"Internal Server Error",http.StatusInternalServerError)
-	}
-	if err := t.Execute(w, data); err != nil {
-		http.Error(w,"Internal Server Error",http.StatusInternalServerError)
-	}
 }
