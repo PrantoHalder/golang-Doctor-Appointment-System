9fedf43e7367a2fa44301d78703923fd655382cc
Pranto Halder <halder2207509@stud.kuet.ac.bd>
2023-03-28 00:50:23 +0600

    quick fix

--- cms/handler/manageSchedule.go
+++ cms/handler/manageSchedule.go
@@ -3,8 +3,12 @@ package handler
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi"
 )
 
 func (h Handler)MangeSchedule(w http.ResponseWriter, r *http.Request){
+	id := chi.URLParam(r,"id")
+	fmt.Println(id)
 	fmt.Println("inside manage schedule ")
 }
--- usermgm/migrations/20230222001101_create_doctor_table.sql
+++ usermgm/migrations/20230222001101_create_doctor_table.sql
@@ -3,15 +3,15 @@
 CREATE TABLE IF NOT EXISTS doctordetails (
 	id BIGSERIAL,
 	userid INT NOT NULL,
-    doctortypeID INT NOT NULL,
+    doctortypeid INT NOT NULL,
 	degree VARCHAR(100) NOT NULL,
 	gender VARCHAR(20) NOT NULL,
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	deleted_at TIMESTAMP DEFAULT NULL,
-
+    UNIQUE (userid),
 	PRIMARY KEY (id),
-	FOREIGN KEY (doctortypeID) REFERENCES doctortype(id),
+	FOREIGN KEY (doctortypeid) REFERENCES doctortype(id),
 	FOREIGN KEY (userid) REFERENCES users(id)
 );
 -- +goose StatementEnd
